5240f022b6adf0f3afd6089c01f81ecb195ebb38
Ladle Patel <ladlepatelgenai@gmail.com>
2025-07-26 03:49:15 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <main class="flex-

--- src/app/(app)/chat/[[...chatId]]/page.tsx
+++ src/app/(app)/chat/[[...chatId]]/page.tsx
@@ -2,7 +2,7 @@ import { ChatPanel } from "@/components/chat/chat-panel";
 
 export default function ChatPage() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col flex-1">
       <ChatPanel />
     </div>
 
